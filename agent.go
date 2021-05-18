f99825a350c81fdf4b670e67f9a0d94201706ecb
nhatthm <nt@hellofresh.com>
2021-05-18 11:18:01 +0200

    Use a forked version of jaeger-client-go to fix security with Thrift

--- agent.go
+++ agent.go
@@ -15,6 +15,7 @@
 package jaeger
 
 import (
+	"context"
 	"fmt"
 	"io"
 	"net"
@@ -73,8 +74,7 @@ func newAgentClientUDP(hostPort string, maxPacketSize int) (*agentClientUDP, err
 // EmitBatch implements EmitBatch() of Agent interface
 func (a *agentClientUDP) EmitBatch(batch *jaeger.Batch) error {
 	a.thriftBuffer.Reset()
-	a.client.SeqId = 0 // we have no need for distinct SeqIds for our one-way UDP messages
-	if err := a.client.EmitBatch(batch); err != nil {
+	if err := a.client.EmitBatch(context.Background(), batch); err != nil {
 		return err
 	}
 	if a.thriftBuffer.Len() > a.maxPacketSize {
--- go.mod
+++ go.mod
@@ -1,8 +1,12 @@
 module contrib.go.opencensus.io/exporter/jaeger
 
+go 1.16
+
 require (
 	github.com/uber/jaeger-client-go v2.25.0+incompatible
 	go.opencensus.io v0.22.4
 	golang.org/x/sync v0.0.0-20200625203802-6e8e738ad208 // indirect
 	google.golang.org/api v0.29.0
 )
+
+replace github.com/uber/jaeger-client-go v2.25.0+incompatible => github.com/nhatthm/jaeger-client-go v2.28.1-0.20210518084659-137f03bd1bdc+incompatible
